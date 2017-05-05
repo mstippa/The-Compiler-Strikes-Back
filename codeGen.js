cee181883ab83304ef4dec78e2c223c5397603e7
Morgan Stippa <Morgan.Stippa1@marist.edu>
2017-05-05 11:24:49 -0400

    started an addition function

--- codeGen.js
+++ codeGen.js
@@ -12,12 +12,13 @@ var idAddress = "";
 
 
 function driver() {
-	generateCode();
-	staticVariables();
+	generateCode(); // generate the code with temporary addresses
+	staticVariables(); // replace the temporary addresses
+	displayCode(); // display the code
 }
 
 
-
+// adds the new line of codes to the executable image
 function addCode () {
 	if (generatedCode.length === 0) {
 		generatedCode = lineCode;
@@ -29,20 +30,23 @@ function addCode () {
 // generates the code
 function generateCode() {
 	var i = 0;
+	var nodeName = "";
 	while (i < astTree.numRootChildren()) {
-		var nodeName = astTree.getBranchNode();
+		nodeName = astTree.getBranchNodeOfRoot();
 		if (nodeName === "VarDecl") {
-				tempCounter++;
-				lineCode = ["A9","00","8D","T"+tempCounter, "XX"];
-				addCode();
-				staticTable[staticTableCounter] = ["T"+tempCounter, astTree.getLeafNode2(), astTree.getLeafNode1(), 0];
-				staticTableCounter++;
+			tempCounter++;
+			lineCode = ["A9","00","8D","T"+tempCounter, "XX"];
+			addCode();
+			staticTable[staticTableCounter] = ["T"+tempCounter, astTree.getLeafNode2(), astTree.getLeafNode1(), 0];
+			staticTableCounter++;
 		} else if (nodeName === "Assign") {
 			// checking for a digit
 			if (digits.indexOf(astTree.getLeafNode2()) > -1) {
 				lineCode = ["A9","0"+astTree.getLeafNode2(), "8D", "T"+tempCounter,"XX"];
 				addCode();
-			// if here then we have not found a digit
+			} else if(astTree.getLeafNode2() === "+") {
+				addition(nodeName);
+			// if here then we have found an id
 			} else {
 				var n = 0;
 				// looping to find the temporary address of an identifier, only looking for ids at the moment
@@ -56,12 +60,16 @@ function generateCode() {
 				}
 			}
 		} else if (nodeName === "print") {
+			console.log(astTree.getLeafNode1());
 			// checking for a char
 			if (chars.indexOf(astTree.getLeafNode1()) > -1 && astTree.getLeafNode1().length < 2) {
 				idAddress = addressLookUp(astTree.getLeafNode1());
 				lineCode = ["AC", idAddress,"XX", "A2", "01", "FF"];
 				addCode();	
-			}		
+			} else if (astTree.getLeafNode1() === "+") {
+				lineCode = addition(nodeName);
+				addCode();
+			} 		
 		}
 		i++;
 	}
@@ -98,8 +106,6 @@ function staticVariables() {
 		staticPositionHex = findStaticPosition(codeLength);
 		i++;
 	}
-	console.log(generatedCode);
-	document.getElementById("codeGen").innerHTML += generatedCode;
 }
 
 
@@ -112,6 +118,41 @@ function addressLookUp(id) {
 		}
 		m++;	
 	}
+}
+
+// displays the code
+function displayCode() {
+	var i = 0;
+	while (i < generatedCode.length) {
+		document.getElementById("codeGen").innerHTML += generatedCode[i] + " ";
+		i++;
+	}
+}
+
+
+function addition(branch) {
+	if (branch === "print") {
+		if (digits.indexOf(astTree.getIntop1(branch)) > -1 && digits.indexOf(astTree.getIntop2(branch)) > -1) {
+			var num = parseInt(astTree.getIntop1(branch))+parseInt(astTree.getIntop2(branch));
+			return ["A0","0"+num.toString(),"A2", "01", "FF"];
+		}
+	} else if (branch === "Assign") {
+		idAddress = addressLookUp(astTree.getLeafNode1()); // get the iddress of the id we are updating
+		// checking if id getting updated is also in the expr
+		if (astTree.getLeafNode1() === astTree.getIntop1(branch) || astTree.getLeafNode1() === astTree.getIntop2(branch)) {
+			tempCounter++;
+			lineCode = ["AD", idAddress, "XX", "8D", "T"+tempCounter, "XX"]; // load the address of the id we are updating and store it in a new location
+			// checking if a digit is the first part of the expr 
+			if (digits.indexOf(astTree.getIntop1(branch)) > -1) {
+				lineCode = lineCode.concat(["A9","0"+astTree.getIntop1(branch)]);
+				lineCode = lineCode.concat(["6D", "T"+tempCounter,"XX", "8D", idAddress, "XX"]);
+				addCode();
+			// if here than the id is the first part of the expr	
+			} else {
+				lineCode = lineCode.concat(["AD","T"+tempCounter, "XX", ]);
+			}
+		}	
+	}	
 }
 
 
